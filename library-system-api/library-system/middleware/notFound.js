315c3c823689bf1401a8692c35762551704c5789
walterjaredco-source <walterjaredco@gmail.com>
2026-05-10 13:13:33 +0800

    Delete library-system-api/library-system directory

--- library-system-api/library-system/middleware/notFound.js
+++
@@ -1,8 +0,0 @@
-const notFound = (req, res) => {
-  res.status(404).json({
-    error: 'Not Found',
-    message: `Route ${req.method} ${req.originalUrl} does not exist.`,
-  });
-};
-
-module.exports = notFound;
